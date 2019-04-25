98b91b7348b0a2cf6be8618eec24b15b10aa1a28
ColinRCasto <Colin@wildheartinteractive.com>
2019-04-24 20:46:20 -0400

    Added scss via vue cli

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.filter("prettyMono", value => {
+  let newStr = value.replace(/o/g, "0");
+  newStr = newStr.replace(/m/g, "M");
+  newStr = newStr.replace(/a/g, "A");
+  return newStr;
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
